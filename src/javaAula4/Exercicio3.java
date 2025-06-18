a47e117200e641626fd3c7415a0a32de77f89da1
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-06-18 14:56:15 -0300

    Ex 3 string

---
+++ src/javaAula4/Exercicio3.java
@@ -0,0 +1,21 @@
+package javaAula4;
+
+public class Exercicio3 {
+	
+	public static void main(String[] args) {
+		
+		String str="Hello World";
+		String resultado = str.substring(6);
+		
+		System.out.println(resultado);
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
