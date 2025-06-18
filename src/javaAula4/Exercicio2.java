92c04471ca44359c4f42572994346300b6dbc08c
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-06-18 14:43:08 -0300

    Ex 2 print, array

---
+++ src/javaAula4/Exercicio2.java
@@ -0,0 +1,18 @@
+package javaAula4;
+import java.util.Arrays;
+
+public class Exercicio2 {
+	
+	public static void main(String[] args) {
+		
+		String A[]= {"G","u","s","t","a","v","o"};
+		
+		for(int i=0;i<A.length;i++)
+		{
+			System.out.print(A[i]+" ");
+		}
+		
+		
+	}
+
+}
