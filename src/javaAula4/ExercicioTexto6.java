b97a06a7748c1360c4dd2bb7e4751a0b898e32ac
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-06-22 15:55:51 -0300

    Ex 6: A vida é bela array to frase string

---
+++ src/javaAula4/ExercicioTexto6.java
@@ -0,0 +1,28 @@
+package javaAula4;
+
+public class ExercicioTexto6 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 6. dado o Array a seguir, {"a", "vida", "é","bela"}
+		faça um programa que crie um string com o texto retirado do Array e imprima. no seguinte formato.
+		A VIDA É BELA
+		 */
+		
+		String texto[]={"a", "vida", "é","bela"};
+		StringBuilder frase= new StringBuilder();
+		
+		for(String word:texto)
+		{
+			frase.append(word).append(" ");
+		}
+		
+		String FraseFinal = frase.toString().trim();
+		
+		System.out.println(FraseFinal);
+		
+
+	}
+
+}
