6dfae9e98dd7e434c4560c614a5416afcca6cca5
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-06-22 15:43:49 -0300

    Texto em Upper

--- src/javaAula4/ExercicioTexto4.java
+++ src/javaAula4/ExercicioTexto4.java
@@ -1,10 +1,24 @@
 package javaAula4;
+import java.util.Scanner;
 
 public class ExercicioTexto4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * 5. Faça um programa que receba de umu suário, um texto e exiba esse texto em letras maiúsculas.
+		 */
+		String texto, converted;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Digite seu texto: ");
+		texto = input.nextLine();
+		converted=texto.toUpperCase();
+		
+		System.out.println(converted);
+		
+		
+		input.close();
+		
 	}
 
 }
