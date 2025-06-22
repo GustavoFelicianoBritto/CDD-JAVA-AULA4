7f22f7ddedb60de4cfd347ffc8c81e0f3f1dadc0
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-06-22 15:26:40 -0300

    Ex de texto 1 e 2

---
+++ src/javaAula4/ExercicioTexto1.java
@@ -0,0 +1,22 @@
+package javaAula4;
+
+public class ExercicioTexto1 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * EXERCÍCIOS
+	1. Faça um programa que, a partir do texto abaixo, “ texto para retirar espaços no
+	início e fim ” e imprima o texto removendo os espaços no inicio e fim do texto.
+		 * 
+		 */
+		
+		String texto = " texto para retirar espaços no início e fim ";
+		String converted = texto.trim();
+		System.out.println(converted);
+
+		
+	}
+	
+
+}
---
+++ src/javaAula4/ExercicioTexto2.java
@@ -0,0 +1,32 @@
+package javaAula4;
+import java.util.Scanner;
+
+public class ExercicioTexto2 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * 3. Faça um programa que, a partir de um texto digitado pelo usuário, conte o número de
+		palavras (palavra é definida por qualquer sequência de caracteres delimitada por espaços em branco) e exiba o resultado.
+		 */
+		String texto;
+		int qtd;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Digite o texto que deseja contar os caracteres: ");
+		texto = input.next();
+		
+		qtd = texto.length();
+		
+		System.out.printf("O texto inserido tem %d caracteres",qtd);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
