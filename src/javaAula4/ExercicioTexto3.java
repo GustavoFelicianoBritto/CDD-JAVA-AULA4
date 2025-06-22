1e664d256f918dadead59fd5da8a2cc3c7957093
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-06-22 15:34:21 -0300

    Texto igual o diferente

---
+++ src/javaAula4/ExercicioTexto3.java
@@ -0,0 +1,17 @@
+package javaAula4;
+
+public class ExercicioTexto3 {
+
+	public static void main(String[] args) {
+		
+		/*compare os 2 textos abaixo e diga se são
+		iguais.*/
+		
+		String texto01 = "Será que são iguais?";
+		String texto02 = "Será que são iguais?";
+		
+		System.out.println(texto01.compareTo(texto02)==0?"Iguais":"Diferentes");
+
+	}
+
+}
