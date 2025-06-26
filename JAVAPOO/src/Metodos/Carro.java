561bb2e6fe4ba4bef9ef1e4a792a36202e05ccbf
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-06-26 16:25:06 -0300

    >

---
+++ JAVAPOO/src/Metodos/Carro.java
@@ -0,0 +1,30 @@
+package Metodos;
+
+public class Carro
+{
+
+	String modelo, cor;
+	double preco;
+
+	public Carro(String m) {
+		this.modelo=m;
+	}
+	public Carro()
+	{
+		
+	}
+	public Carro(String m,String c,double p)
+	{
+		this.cor=c;
+		this.modelo=m;
+		this.preco=p;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ JAVAPOO/src/Metodos/LojadeCarros.java
@@ -0,0 +1,16 @@
+package Metodos;
+
+public class LojadeCarros {
+
+	public static void main(String[] args)
+	{
+		Carro c1 = new Carro("Fusca");
+		Carro c2 = new Carro("Fusca 2");
+		
+		Carro c3 = new Carro("Bot", "Azul", 56.23) ;
+		System.out.println(c3.cor);
+		
+	}
+	
+	
+}
